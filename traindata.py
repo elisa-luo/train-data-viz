5ce7dd2a2de739cfa36edadc6f2bfb7ad6089d47
Elisa Luo <eyl2130@columbia.edu>
2020-05-20 20:36:41 -0700

    started working on averages and totals section. Cleaned up some formatting for the rest of the code.

--- traindata.py
+++ traindata.py
@@ -12,9 +12,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #PARSE THE DATA!
-train = pd.read_csv("train.csv")
+train = pd.read_csv("train2.csv")
 train["date"] = pd.to_datetime(train["date"])
 
+#MAKE ADJUSTED DISTANCE COLUM (that's not how u spell it but whatev)
 adj_dist = []
 
 for i in range(0,len(train["mode"])):
@@ -32,7 +33,7 @@ train["adj_dist"] = adj_dist
 #GROUP BY DATE AND DISTANCE
 dist_by_date = train[["date","mode","distance"]].groupby(["date","mode"]).sum()
 dist_by_date = dist_by_date.unstack()
-dist_by_date.plot.bar(stacked = True)
+#dist_by_date.plot.bar(stacked = True)
 
 #GROUP BY DATE AND ADJUSTED DISTANCE
 dist_by_date = train[["date","mode","adj_dist"]].groupby(["date","mode"]).sum()
@@ -50,24 +51,29 @@ new.plot.bar(stacked = True)
 
 #GROUP BY MODE AND TIME_ELAPSED
 mode_per_time = train[["mode", "time_elapsed"]].groupby("mode").sum()
-mode_per_time.plot.pie(subplots=True, figsize = (6,6))
+#mode_per_time.plot.pie(subplots=True, figsize = (6,6))
 
 
 #GROUP BY MODE AND DISTANCE!
 mode_per_dist = train[["mode", "distance"]].groupby("mode").sum()
-mode_per_dist.plot.pie(subplots=True, figsize = (6,6))
+#mode_per_dist.plot.pie(subplots=True, figsize = (6,6))
 
 #MODE AND ADJUSTED DISTANCE
 adj_mode_per_dist = train[["mode","adj_dist"]].groupby("mode").sum()
 adj_mode_per_dist.plot.pie(subplots=True, figsize = (6,6))
 
-    
-
 
 ########################################################
 #TOTALS & AVERAGES SECTION!
 
+#TODO: fix floating point number issue
+date_range = (train["date"].max() - train["date"].min()).days
+
 #TOTAL MINUTES
+tmin = mode_per_time.sum()[0]
+thour = tmin/60
+tday = thour/24
+print("You spent a total of {} minutes training over a period of {} days! That's {} hours or {} days!".format(tmin,date_range,thour,tday))
 
 #TOTAL DISTANCE
 
